1fdb7c2cb0ef7ef73541e4a04e4aa6681b21997b
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 16:48:13 +0200

    klasy obiektow

--- js/obiekty.js
+++ js/obiekty.js
@@ -17,3 +17,32 @@ var domi={
 
 kaja.przedstawOsobe();
 domi.przedstawOsobe();
+
+//klasa to funkcja do tworzenia nowych obiektow
+
+function Osoba(imie, nazwisko)
+ this.imie=imie;
+ this.nazwisko=nazwisko
+ this.wyswietlInfo //tworzymy klase ogolną z 3 eelemntami generalnymi
+
+
+
+var krystian=new Osoba(krystian, dziopa)//tworzymy nową osobę ktora bedzie sie odowolywac do wyzej stworzonej klasy obiektow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
